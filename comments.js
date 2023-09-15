5286c7c1e9950365cbedb81af0b0d284511d828a
misenkashari <63194411+misenkashari@users.noreply.github.com>
2023-09-15 08:25:48 +0000

    adding comments.js

--- comments.js
+++ comments.js
@@ -1,61 +1,47 @@
-// create web server with express
-const express = require('express');
-const app = express();
-// create a port
-const port = 3000;
-// create a path
-const path = require('path');
-// create a body parser
-const bodyParser = require('body-parser');
-// create a mongoose
-const mongoose = require('mongoose');
-// connect to mongoose
-mongoose.connect('mongodb://localhost:27017/comments', { useNewUrlParser: true, useUnifiedTopology: true });
-// create a schema
-const commentSchema = new mongoose.Schema({
-    name: String,
-    comment: String
-});
-// create a model
-const Comment = mongoose.model('Comment', commentSchema);
-// use body parser
-app.use(bodyParser.urlencoded({ extended: true }));
-// use static files
-app.use(express.static('public'));
-// set view engine
-app.set('view engine', 'ejs');
-// set views
-app.set('views', path.join(__dirname, 'views'));
-// get home page
-app.get('/', (req, res) => {
-    res.render('home');
-});
-// get comments page
-app.get('/comments', (req, res) => {
-    Comment.find({}, (err, comments) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('comments', { comments: comments });
-        }
-    });
-});
-// post comments page
-app.post('/comments', (req, res) => {
-    const comment = new Comment({
-        name: req.body.name,
-        comment: req.body.comment
-    });
-    comment.save((err, comment) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(comment);
-            res.redirect('/comments');
-        }
-    });
-});
-// listen to port
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// Create web server
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+var comments = [];
+http.createServer(function(req,res){
+	var objUrl = url.parse(req.url,true);
+	var pathname = objUrl.pathname;
+	//get 请求
+	if(pathname == '/'){
+		fs.readFile('./index.html',function(err,data){
+			if(err){
+				throw err;
+			}
+			var str = data.toString();
+			var str2 = '';
+			comments.forEach(function(item){
+				str2 += '<li>'+item+'</li>';
+			});
+			str = str.replace('<%comments%>',str2);
+			res.end(str);
+		});
+	}else if(pathname == '/add'){
+		//post 请求
+		var str = '';
+		req.on('data',function(chunk){
+			str += chunk;
+		});
+		req.on('end',function(){
+			comments.push(str);
+			res.end(JSON.stringify(comments));
+		});
+	}else{
+		var file = './'+pathname;
+		var extname = path.extname(file);
+		fs.readFile(file,function(err,data){
+			if(err){
+				res.end('404');
+			}
+			getMime(extname,function(mime){
+				res.writeHead(200,{"Content-Type":mime});
+				res.end(data);
+			});
+		});
+	}
+}).listen(8080,'
